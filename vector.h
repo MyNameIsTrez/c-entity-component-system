aa0504b3c0252c8b373ad9e00887f039c4e70c44
MyNameIsTrez <welfje@gmail.com>
2023-04-10 21:11:57 +0200

    Rename vector_push() to vector_grow()

--- vector.h
+++ vector.h
@@ -7,16 +7,20 @@
 typedef struct s_vector	t_vector;
 
 t_vector	*vector_new(size_t type_size);
-void		vector_push(t_vector *vector);
+void		vector_grow(t_vector *vector);
 void		*vector_get(t_vector *vector, size_t index);
 void		vector_free(t_vector *vector);
 
+#endif // VECTOR_H
+
 # ifdef VECTOR_IMPLEMENTATION
+# define VECTOR_IMPLEMENTATION
 
 struct s_vector
 {
 	size_t	type_size;
 	size_t	element_count;
+	size_t	element_capacity;
 	uint8_t	*address;
 };
 
@@ -29,18 +33,31 @@ t_vector	*vector_new(size_t type_size)
 	return (vector);
 }
 
-void	vector_push(t_vector *vector)
+void	vector_grow(t_vector *vector)
 {
+	size_t	new_element_capacity;
 	uint8_t	*new_address;
 
-	vector->element_count++;
-	new_address = calloc(vector->element_count, vector->type_size);
-	if (vector->element_count > 1)
+	if (vector->element_count + 1 > vector->element_capacity)
 	{
-		memcpy(new_address, vector->address, (vector->element_count - 1) * vector->type_size);
+		if (vector->element_capacity == 0)
+		{
+			new_element_capacity = 1;
+		}
+		else
+		{
+			new_element_capacity = vector->element_capacity * 2;
+		}
+		new_address = calloc(new_element_capacity, vector->type_size);
+		if (vector->element_count > 0)
+		{
+			memcpy(new_address, vector->address, vector->element_count * vector->type_size);
+		}
+		free(vector->address);
+		vector->address = new_address;
+		vector->element_capacity = new_element_capacity;
 	}
-	free(vector->address);
-	vector->address = new_address;
+	vector->element_count++;
 }
 
 void	*vector_get(t_vector *vector, size_t index)
@@ -55,5 +72,3 @@ void	vector_free(t_vector *vector)
 }
 
 # endif // VECTOR_IMPLEMENTATION
-
-#endif // VECTOR_H
