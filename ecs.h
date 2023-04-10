4c9725a04887016e3149cf2dc6dabe42cb1b06b4
MyNameIsTrez <welfje@gmail.com>
2023-04-10 22:50:33 +0200

    Add _find_archetype()

--- ecs.h
+++ ecs.h
@@ -85,7 +85,8 @@ struct s_iterator
 
 t_ecs   *ecs_init(size_t t_c_size, size_t t_g_size)
 {
-	t_ecs   *ecs;
+	t_ecs   	*ecs;
+	t_archetype	*archetype;
 
 	ecs = calloc(1, sizeof(t_ecs));
 
@@ -98,14 +99,15 @@ t_ecs   *ecs_init(size_t t_c_size, size_t t_g_size)
 
 	ecs->archetypes = vector_new(sizeof(t_archetype));
 	vector_grow(ecs->archetypes);
-	((t_archetype *)vector_get(ecs->archetypes, 0))->c = calloc(1, t_c_size);
-	((t_archetype *)vector_get(ecs->archetypes, 0))->g = calloc(1, t_g_size);
+	archetype = vector_get(ecs->archetypes, 0);
+	archetype->c = calloc(1, t_c_size);
+	archetype->g = calloc(1, t_g_size);
 
     ecs->new_entity_archetype = calloc(1, sizeof(t_archetype));
     ecs->new_entity_archetype->c = calloc(1, ecs->t_c_size);
 	ecs->new_entity_archetype->g = calloc(1, ecs->t_g_size);
 
-	return (ecs);
+	return ecs;
 }
 
 void    ecs_size(t_ecs *ecs, t_c *sizes)
@@ -125,11 +127,13 @@ void    ecs_size(t_ecs *ecs, t_c *sizes)
 t_entity_id	ecs_entity(t_ecs *ecs)
 {
 	t_archetype *empty_archetype;
+	t_archetype	**archetype_ptr;
 
 	empty_archetype = vector_get(ecs->archetypes, 0);
 	vector_grow(ecs->entity_id_archetype_pairs);
-	*((t_archetype **)vector_get(ecs->entity_id_archetype_pairs, ecs->next_highest_entity_id)) = empty_archetype;
-	return (ecs->next_highest_entity_id++);
+	archetype_ptr = vector_get(ecs->entity_id_archetype_pairs, ecs->next_highest_entity_id);
+	*archetype_ptr = empty_archetype;
+	return ecs->next_highest_entity_id++;
 }
 
 static void	_update_new_entity_archetype(t_archetype *old_entity_archetype, t_c *added_component, t_ecs *ecs)
@@ -150,36 +154,92 @@ static void	_update_new_entity_archetype(t_archetype *old_entity_archetype, t_c 
 	}
 }
 
+static bool	_c_identical(t_c *c, t_c *new_c, size_t t_c_size)
+{
+	size_t		index;
+
+	index = 0;
+	while (index < t_c_size)
+	{
+		if (((size_t *)c)[index] != ((size_t *)new_c)[index])
+		{
+			return false;
+		}
+		index++;
+	}
+	return true;
+}
+
+static bool	_g_identical(t_g *g, t_g *new_g, size_t t_g_size)
+{
+	size_t		index;
+
+	index = 0;
+	while (index < t_g_size)
+	{
+		if (((bool *)g)[index] != ((bool *)new_g)[index])
+		{
+			return false;
+		}
+		index++;
+	}
+	return true;
+}
+
+static t_archetype	*_find_archetype(t_archetype *new_entity_archetype, t_vector *archetypes, size_t t_c_size, size_t t_g_size)
+{
+	size_t		index;
+	t_archetype	*archetype;
+
+	index = 0;
+	while (index < vector_size(archetypes))
+	{
+		archetype = vector_get(archetypes, index);
+		if (_c_identical(archetype->c, new_entity_archetype->c, t_c_size)
+		&& _g_identical(archetype->g, new_entity_archetype->g, t_g_size))
+		{
+			return vector_get(archetypes, index);
+		}
+		index++;
+	}
+	return NULL;
+}
+
 void	ecs_component(t_ecs *ecs, t_entity_id entity_id, t_c *added_component, void *value)
 {
-	t_archetype	*old_entity_archetype;
+	t_archetype	**old_entity_archetype_ptr;
+	t_archetype	*found_archetype;
 
-	old_entity_archetype = *(t_archetype **)vector_get(ecs->entity_id_archetype_pairs, entity_id);
-	_update_new_entity_archetype(old_entity_archetype, added_component, ecs);
-	(void)old_entity_archetype;
-	(void)added_component;
+	old_entity_archetype_ptr = vector_get(ecs->entity_id_archetype_pairs, entity_id);
+	_update_new_entity_archetype(*old_entity_archetype_ptr, added_component, ecs);
 	(void)value;
 
 	// Checks whether new_entity_archetype.c and .g are already in ecs.archetypes
-	// if (ecs->new_entity_archetype in ecs->archetypes)
-	// {
-	// 	ecs->entity_id_archetype_pairs[entity_id] = existing_archetype
-	// }
-	// else
-	// {
+	found_archetype = _find_archetype(ecs->new_entity_archetype, ecs->archetypes, ecs->t_c_size, ecs->t_g_size);
+	if (found_archetype == NULL)
+	{
 	// 	new_archetype = calloc();
 	// 	ft_memcpy(new_archetype, ecs->new_entity_archetype, );
 	// 	ecs->archetypes vector_push() new_archetype
 	// 	ecs->entity_id_archetype_pairs[entity_id] = new_archetype
-	// }
+	//  use value
+	}
+	else
+	{
+		*old_entity_archetype_ptr = found_archetype;
+	}
 }
 
 void	ecs_cleanup(t_ecs *ecs)
 {
+	t_archetype	*archetype;
+
 	free(ecs->component_sizes);
 	vector_free(ecs->entity_id_archetype_pairs);
-	free(((t_archetype *)vector_get(ecs->archetypes, 0))->c);
-	free(((t_archetype *)vector_get(ecs->archetypes, 0))->g);
+	// TODO: Loop over all archetypes to free them all!
+	archetype = vector_get(ecs->archetypes, 0);
+	free(archetype->c);
+	free(archetype->g);
 	vector_free(ecs->archetypes);
 	free(ecs->new_entity_archetype->c);
 	free(ecs->new_entity_archetype->g);
